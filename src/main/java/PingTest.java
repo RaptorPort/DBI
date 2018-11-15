6b9970ac3277935389f0288baecce89efb43f908
Nico <nico.meissen@gmail.com>
2018-11-15 10:33:49 +0100

    Connection to Server successfull

--- src/main/java/PingTest.java
+++ src/main/java/PingTest.java
@@ -7,13 +7,14 @@ public class PingTest {
 
 	private static final String USERNAME = "dbi";
 	private static final String PASSWORD = "dbi_pass";
-	private static final String CONN_STRING = "jdbc:mysql://192.168.15.30/cpa";
+	private static final String CONN_STRING = "jdbc:mysql://192.168.122.117/cap?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	
 	public static void main(String[] args) throws SQLException {
 		
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("Connected!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
