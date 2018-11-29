f9eaee859696b184fef4ab6a5861cf9adfcf36c5
Nico <nico.meissen@gmail.com>
2018-11-29 12:08:26 +0100

    I'm not dumb I swear

--- src/main/java/PingTest.java
+++ src/main/java/PingTest.java
@@ -333,12 +333,52 @@ public class PingTest {
 	}
 	
 	public static void init_tps_DBinline(Connection conn, int n) throws SQLException {
-		Statement stmt = conn.createStatement();
+		Random zufall = new Random(); // neues Random Objekt, namens zufall
+		 
+		final String ADDRESS68 = "ADRESSE6812345678912345678912345678912345678912345678912345678912345";
+		final String ADDRESS72 = "ADRESSE72123456789123456789123456789123456789123456789123456789123456789";
+		final String NAME20 = "NAME2001234567890123";
+		final String CMMNT30 = "CMMNT1234567891234567891234567";
+		
+		PreparedStatement stmt = conn.prepareStatement( 
+				"insert into branches values (?, 'BRANCHNAME', 0, 'ADDRESS')"
+				);
+		//n Tupel in der BRANCH-Relation mit fortlaufender BRANCHID (1 bis n), der
+		//BALANCE 0 und Strings der richtigen Länge für BRANCHNAME und ADDRESS
+		for (int i = 1; i <= n; i++) {
+			stmt.setInt(1, i);
+			stmt.executeUpdate();
+		}
+		conn.commit();
+		System.out.println("Branches DONE");
+	
+		//n * 100000 Tupel in der ACCOUNTS-Relation mit fortlaufender ACCID (1 bis
+		//n * 100000), dem Kontostand (BALANCE) 0, einer zufälligen BRANCHID (1 bis n) und
+		//wieder beliebigen Strings der richtigen Länge für NAME und ADDRESS
+		
+		Statement stmt2 = conn.createStatement();
 		System.out.println("Start INLINE insert");
-		stmt.executeUpdate("LOAD DATA LOCAL INFILE 'INFILEaccounts.csv' INTO TABLE test.accounts;");
+		stmt2.executeUpdate("LOAD DATA LOCAL INFILE 'INFILEaccounts.csv' INTO TABLE test.accounts;");
 		conn.commit();
 		System.out.println("Commit check");
 		
+		System.out.println("Accounts DONE");
+		//n * 10 Tupel in der TELLER-Relation mit fortlaufender TELLERID (1 bis n * 10), der
+		//BALANCE 0, einer zufälligen BRANCHID (1 bis n) und wieder beliebigen Strings der
+		//richtigen Länge für TELLERNAME und ADDRESS
+		stmt = conn.prepareStatement( 
+				"insert into tellers values (?, ?, 0, ?, ?)"
+				);
+		for (int i = 1; i <= n*10; i++) {
+			stmt.setInt(1, i);
+			stmt.setString(2, NAME20);
+			stmt.setInt(3, (int)(zufall.nextDouble()*n+1));
+			stmt.setString(4, ADDRESS68);
+			stmt.executeUpdate();
+		}
+		conn.commit(); 
+		System.out.println("Tellers DONE");
+		//0 Tupel in der HISTORY-Relation.
 		
 	}
 }
