34f256010916549c3da90f4834a3f6b6482dec6f
SvenMitt <svenm1997@gmx.de>
2018-12-13 11:06:39 +0100

    Kontostand

--- src/main/java/tx/Kontostand.java
+++ src/main/java/tx/Kontostand.java
@@ -1,5 +1,23 @@
 package tx;
 
-public class Kontostand {
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
+public class Kontostand {
+	static Connection con = null;
+	static PreparedStatement stmt = null;
+	
+	public static int kontostand (int accid) {
+		try {
+			stmt = con.prepareStatement("SELECT balance FROM accounts WHERE accid = "+accid+";");
+			ResultSet result = stmt.executeQuery();
+			result.next();
+			return result.getInt(1);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
 }
