c36511dec3301719d54f8dcbd40474ac6d3bb243
SvenMitt <svenm1997@gmx.de>
2018-12-13 11:06:07 +0100

    methoden fertig

--- src/main/java/tx/Analyse.java
+++ src/main/java/tx/Analyse.java
@@ -1,5 +1,26 @@
 package tx;
 
-public class Analyse {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class Analyse {
+	static Connection con = null;
+	static PreparedStatement stmt = null;
+	
+	static int analyse (int delta){
+	try {
+		stmt = con.prepareStatement("SELCET = FROM histroy WHERE delta =" +delta+ ";");
+		ResultSet result = stmt.executeQuery();
+		int Anzahl = 0;
+		while(result.next())
+			Anzahl++;
+		return Anzahl;
+		}
+	catch (Exception e){
+		e.printStackTrace();
+		return 0;
+		}
+	
+	}
 }
