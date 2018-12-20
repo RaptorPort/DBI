f8a657eb7b347c6b447357176c3c1d9d90baa9cd
DanielG <daniel.g-h@gmx.de>
2018-12-20 10:09:26 +0100

    ip change

--- src/main/java/Benchmark.java
+++ src/main/java/Benchmark.java
@@ -14,7 +14,7 @@ public class Benchmark {
 
 	private static final String USERNAME = "dbi";
 	private static final String PASSWORD = "dbi_pass";
-	private static final String CONN_STRING = "jdbc:mysql://192.168.122.117/test?allowLoadLocalInfile=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+	private static final String CONN_STRING = "jdbc:mysql://192.168.122.54/test?allowLoadLocalInfile=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
 			+"&sessionVariables=unique_checks=0,foreign_key_checks=0&useCompression=true"; //Session Variablen setzen
 	
 	public static void main(String[] args) throws SQLException, InterruptedException {
